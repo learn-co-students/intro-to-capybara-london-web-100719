3b5f70e75a53f2e39cf5528f7c3da91bdf59c947
Ned-el-ch <nedelchev.niki@gmail.com>
2019-10-28 13:46:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 class Application < Sinatra::Base
-  # Write your code here!
 
-end
+    get '/' do
+
+        erb :index
+
+    end
+
+    post '/greet' do
+
+        erb :greet
+
+    end
+
+  end
